b55270b4d53f82c4b87ec150d70c2f4fd313260f
Marius Strumyla <98410601+MariusStrumyla@users.noreply.github.com>
2022-05-03 21:03:36 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [showGreeting, setShowGreeting] = useState(false);
+  const [name, setName] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <input
+              type="text"
+              onChange={(e) => {
+                  setName(e.target.value);
+              }}
+          />
+          <button
+              onClick={() => setShowGreeting(!showGreeting)}>{!showGreeting ?  "Hide": "Show"}
+          </button>
+             <p>{showGreeting ? `Hi ${name}` : `` }</p>    
+      </>
   );
-}
+};
+
 
 export default App;
